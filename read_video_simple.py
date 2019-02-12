b2a14ac14592e973e4340f9bb8542fc673642664
Sehul Viras <sehul.viras@live.com>
2019-02-12 15:00:23 -0600

    Update read_video_simple.py

--- read_video_simple.py
+++ read_video_simple.py
@@ -1,6 +1,9 @@
 import cv2
 
-cap = cv2.VideoCapture('video.MOV')
+cap = cv2.VideoCapture(0)
+//To use internal webcam, use 0 (e.g. cap = cv2.VideoCapture(0))
+//To use external usb webcam, replace 0 with 1 (e.g. cap = cv2.VideoCapture(1))
+//To play a saved video, just replace the 0 or 1 with the name of the video file.  (e.g. cap = cv2.VideoCapture('video.MOV))
 
 while True:
     ref, frame = cap.read()
